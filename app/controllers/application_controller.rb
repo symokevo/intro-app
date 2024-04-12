3d8a39c67e5b8f18e2c94586bca2e41e0cc653c1
Simon Rabuogi <symokevo@gmail.com>
2024-04-12 12:36:55 +0300

    Improve the README file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "Hello world!"
   end
+
+  # Goodbye action
+  def goodbye
+    render html: "Goodbye world!"
+  end
 end
